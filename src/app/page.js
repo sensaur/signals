41eb169882597d7d95ae2689035a4ecdb767b0ed
Ilya Zhigarev <zhigarev@bk.ru>
2024-01-16 11:08:05 +0100

    feat: add TodoListPlain

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,5 @@
 import Container from '@mui/material/Container';
 import NavBar from "@/components/NavBar";
-import TodoListMUI from "@/components/TodoListMUI";
-import TodoListPlain from "@/components/TodoListPlain";
-import TodoListPlainRep from "@/components/TodoListPlainRep";
 import TodoList from "@/components/TodoList";
 
 export default function Home() {
