a9ed119c548a8cd17914b048315a17355f070c65
Ilya Zhigarev <zhigarev@bk.ru>
2024-01-16 12:39:39 +0100

    feat: add TodoListPlain

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,42 +2,43 @@
 
 import {useState} from "react";
 
-export default function () {
+export default function TodoList() {
   const [newTodo, setNewTodo] = useState("")
   const [todos, setTodos] = useState([])
-
   const handleAddTodo = () => {
     if (newTodo) {
-      setTodos(() => [...todos, {text: newTodo, isDone: false}])
+      setTodos([...todos, {text: newTodo, isDone: false}])
       setNewTodo('')
     }
   }
 
-  const toggleTodo = (i) => {
+  const handleToggle = (i) => {
     const newTodos = [...todos]
     newTodos[i].isDone = !newTodos[i].isDone
     setTodos(newTodos)
   }
 
+  console.log("todo", todos)
 
   return (
     <div>
       <input
-        placeholder="New Todo"
+        type="text"
+        placeholder="New TODO"
         value={newTodo}
         onChange={(e) => setNewTodo(e.target.value)}
       />
-      <button
-        onClick={handleAddTodo}
-      >Add todo
-      </button>
-      <ul style={{listStyleType: "none"}}>
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <ul>
         {todos.map((el, i) => (
-          <li key={i} style={{}}>
+          <li
+            key={i}
+            onClick={() => handleToggle(i)}
+            style={{listStyleType: "none"}}
+          >
             <input
               type="checkbox"
               checked={el.isDone}
-              onClick={() => toggleTodo(i)}
               onChange={() => {
               }}
             />
